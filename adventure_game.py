44cf16d0bf4fb7553a30d7a32fc7a2a5eebec5e6
osouza <mauricio.souza002@gmail.com>
2022-10-24 10:42:57 -0200

    Corrigir linha de input da função estábulo.

--- adventure_game.py
+++ adventure_game.py
@@ -5,7 +5,7 @@ bag = []
 
 def print_pause(message):
     print(message)
-    time.sleep(2)
+    time.sleep(1)
 
 def stable():
     print_pause("The horse groom scowl at you. The service will "
@@ -13,12 +13,12 @@ def stable():
     print_pause("You open your bag and see exactly 1 silver dollar.")
     print_pause("Enter 1 to pay the man.")
     print_pause("Enter 2 to bargain with him.")
-    response = print_pause(input("What you will do?\n"))
+    response = input("What you will do?\n")
     if response == "1":
         print_pause("You pay the man and leave the stable. "
                     "The horse groom will take care of your "
                     "stallion.")
     elif response == "2":
-        print_pause("")
-          
+        print_pause("The man's face turns red as he stand up 
+
 stable()
