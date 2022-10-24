c95c456cabe5d9276a43fa1264d481749d0660ca
osouza <mauricio.souza002@gmail.com>
2022-10-24 16:52:19 -0200

    Adicionar função gunshop.

--- adventure_game.py
+++ adventure_game.py
@@ -14,6 +14,18 @@ def intro():
                 "through the desert.")
     main_street()
 
+def continue_quit():
+    while True:
+        print_pause("Would you like to play again?")
+        print_pause("\nEnter 1 if you want to play again.")
+        print_pause("Enter 2 if you want to quit this masterpiece.")
+        response = input("What will you do?\n")
+        if response == "1":
+            intro()
+        elif response == "2":
+            print_pause("Good bye!")
+            break
+
 def main_street():
     while True:
         print_pause("You look around and see some buildings: a stable, "
@@ -24,19 +36,19 @@ def main_street():
         print("What will you do?")
         response = input("(Please enter 1, 2 or 3.)\n")
         if response == "1":
-            print_pause("You walk to the stable.")
+            print_pause("You enter the stable.")
             stable()
         elif response == "2" and "horse" in items:
             print_pause("You can't enter the saloon while riding a horse.")
             print_pause("You turn around and go back to the main street.")
         elif response == "2" and "horse" not in items:
-            print_pause("You walk to the saloon")
+            print_pause("You enter the saloon")
             # saloon()       
         elif response == "3" and "horse" in items:
             print_pause("You can't enter the gunshop while riding a horse.")
             print_pause("You turn around and go back to the main street.")
         elif response == "3" and "horse" not in items:
-            print_pause("You walk to the gunshop.")
+            print_pause("You enter the gunshop.")
             # gunshop()
         else:
             print_pause("Please enter a valid number")       
@@ -68,12 +80,19 @@ def stable():
                 print_pause("Please enter a valid number.")
                 stable()
 
+def gunshop():
+    while True:
+        print_pause("The clerk at the counter welcomes you.")
+        print_pause("He shows you a beautiful gun.")
+        print_pause("It costs just 1 silver dollar. Do you ")                
+
 def saloon():
+    duel_result = random.choice("win", "lose")
     while True:
         if "gun" not in items:
             print_pause("The bouncer looks at you and tells you "
-                        "need a gun in order to access the saloon. "
-                        "Have you lost your mind?")
+                        "that you need a gun in order to access the "
+                        "saloon. Have you lost your mind?")
             main_street()            
         else:
             print_pause("You enter the saloon.")
@@ -83,6 +102,18 @@ def saloon():
             print_pause("Enter 2 to decline.")
             print_pause("What will you do?")
             response = input("(Please enter 1 or 2.)")
+            if response == 1:
+                print_pause("Your draw your gun...")
+                print_pause("...")
+                if duel_result == "win":
+                    print_pause("... and quickly shot the man dead.")
+                    print_pause("Congratulations... you won the game!")
+                    continue_quit()
+                elif duel_result == "lose":
+                    print_pause("... but you can't do it in time.")
+                    print_pause("You are dead. Game over!")
+                    continue_quit()    
+                    
     
 
 intro()
