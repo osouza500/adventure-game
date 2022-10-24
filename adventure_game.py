8547816888b59101dac20c6d5751781ced337869
osouza <mauricio.souza002@gmail.com>
2022-10-24 10:33:32 -0200

    Definir função estábulo.

--- adventure_game.py
+++ adventure_game.py
@@ -1,5 +1,24 @@
 import time
+import random
+
+bag = []
 
 def print_pause(message):
     print(message)
     time.sleep(2)
+
+def stable():
+    print_pause("The horse groom scowl at you. The service will "
+                "cost you 1 silver dollar.")
+    print_pause("You open your bag and see exactly 1 silver dollar.")
+    print_pause("Enter 1 to pay the man.")
+    print_pause("Enter 2 to bargain with him.")
+    response = print_pause(input("What you will do?\n"))
+    if response == "1":
+        print_pause("You pay the man and leave the stable. "
+                    "The horse groom will take care of your "
+                    "stallion.")
+    elif response == "2":
+        print_pause("")
+          
+stable()
