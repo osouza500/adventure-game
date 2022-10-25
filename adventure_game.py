0f4eb475c6860e911a918414b6070231c9f39e1a
osouza <mauricio.souza002@gmail.com>
2022-10-25 13:10:23 -0200

    Corrigir formatação baseado em pycodestyle.

--- adventure_game.py
+++ adventure_game.py
@@ -1,24 +1,28 @@
 import time
 import random
 
+
 def print_pause(message):
     print(message)
     time.sleep(0)
 
+
 def play_game():
     items = ["horse"]
     intro(items)
-    main_street(items)    
+    main_street(items)
+
 
 def intro(items):
     if "horse" not in items and "gun" in items:
         items.append("horse")
-        items.remove("gun")   
+        items.remove("gun")
     print_pause("You arrive at the main street of the old mining town.")
     print_pause("The sun is setting down.")
     print_pause("Your horse looks tired from the three-day journey "
                 "through the desert.")
     main_street(items)
+
 
 def continue_quit(items):
     print_pause("Would you like to play again?")
@@ -30,17 +34,19 @@ def continue_quit(items):
         intro(items)
     elif response == "2":
         print_pause("Good bye!")
+        quit()
     else:
         print_pause("Please enter a valid number.")
         continue_quit(items)
 
+
 def main_street(items):
     print_pause("You are in the main street.")
     print_pause("You look around and see some buildings: a "
-                "stable, a saloon and a gun shop.") 
-    print_pause("\nEnter 1 to walk to the stable.")
-    print_pause("Enter 2 to walk to the saloon.")
-    print_pause("Enter 3 to walk to the gunshop.")
+                "stable, a saloon and a gun shop.")
+    print_pause("\nEnter 1 to enter the stable.")
+    print_pause("Enter 2 to enter the saloon.")
+    print_pause("Enter 3 to enter the gun shop.")
     print("What will you do?")
     response = input("(Please enter 1, 2 or 3.)\n")
     if response == "1":
@@ -51,29 +57,30 @@ def main_street(items):
                     "a horse.")
         print_pause("You turn around and go back to the main "
                     "street.")
-        main_street(items)            
+        main_street(items)
     elif response == "2" and "horse" not in items:
         print_pause("You enter the saloon")
-        saloon(items)       
+        saloon(items)
     elif response == "3" and "horse" in items:
-        print_pause("You can't enter the gunshop while "
+        print_pause("You can't enter the gun shop while "
                     "riding a horse.")
         print_pause("You turn around and go back to the main "
                     "street.")
-        main_street(items)            
+        main_street(items)
     elif response == "3" and "horse" not in items:
-        print_pause("You enter the gunshop.")
-        gunshop(items)
+        print_pause("You enter the gun shop.")
+        gun_shop(items)
     else:
         print_pause("Please enter a valid number")
-        main_street(items)       
+        main_street(items)
+
 
 def stable(items):
     if "horse" not in items:
         print_pause("Your horse is already here.")
         print_pause("You turn around and go back to the main street.")
         main_street(items)
-    else:   
+    else:
         print_pause("The horse groom scowl at you. The service will "
                     "cost you 1 silver dollar.")
         print_pause("\nEnter 1 to pay the man.")
@@ -84,8 +91,8 @@ def stable(items):
             print_pause("You pay the man and leave the stable. "
                         "The horse groom will take care of your "
                         "stallion.")
-            items.remove("horse")            
-            main_street(items)                         
+            items.remove("horse")
+            main_street(items)
         elif response == "2":
             print_pause("You leave the stable thinking about your avarice.")
             main_street(items)
@@ -93,12 +100,13 @@ def stable(items):
             print_pause("Please enter a valid number.")
             stable(items)
 
-def gunshop(items):
+
+def gun_shop(items):
     if "gun" in items:
         print_pause("You already have a gun.")
         print_pause("You turn around and go back to the "
                     "main street.")
-        main_street(items)            
+        main_street(items)
     print_pause("The clerk at the counter welcomes you.")
     print_pause("He shows you a beautiful gun.")
     print_pause("It costs 1 silver dollar. Do you want "
@@ -113,11 +121,12 @@ def gunshop(items):
         main_street(items)
     elif response == "2":
         print_pause("You leave the gunshop thinking about your "
-                    "avarice.")  
-        main_street(items)              
+                    "avarice.")
+        main_street(items)
     else:
         print_pause("Please enter a valid number.")
-        gunshop(items)
+        gun_shop(items)
+
 
 def saloon(items):
     duel_result = random.choice(["win", "lose"])
@@ -126,7 +135,7 @@ def saloon(items):
                     "that you need a gun in order to access the "
                     "saloon.")
         print_pause("Have you lost your mind?")
-        main_street(items)            
+        main_street(items)
     if "gun" in items:
         print_pause("The place is crowded with crooks.")
         print_pause("One of them challenges you to a duel.")
@@ -151,6 +160,7 @@ def saloon(items):
             main_street(items)
         else:
             print_pause("Please enter a valid number.")
-            saloon(items)                    
-                    
+            saloon(items)
+
+
 play_game()
